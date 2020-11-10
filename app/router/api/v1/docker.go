18c630b651571cc5d1384b1f5c52c0df795b0e9a
Aurora <877286291@qq.com>
2020-11-10 17:22:25 +0800

    2020-11-10 first commit

--- app/router/api/v1/docker.go
+++ app/router/api/v1/docker.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"DockerUI/app/models"
 	"DockerUI/app/service"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
@@ -10,20 +12,43 @@ func Info(c *gin.Context) {
 	host := c.Query("host")
 	info, err := service.Info(host)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-	} else {
-		c.JSON(http.StatusOK, info)
+		log.Println(err)
 	}
+	infoList := make([]interface{}, 0)
+	response := models.Response{
+		Code:    0,
+		Message: "",
+		Data:    nil,
+	}
+	if err != nil {
+		response.Code = http.StatusInternalServerError
+		response.Message = "Docker主机信息获取失败！"
+	} else {
+		infoList = append(infoList, info)
+		response.Code = http.StatusOK
+		response.Message = "Docker主机信息获取成功！"
+		response.Data = infoList
+	}
+	c.JSON(http.StatusOK, response)
 }
 
 func ImageList(c *gin.Context) {
 	host := c.Query("host")
 	images, err := service.ImageList(host)
 	infoList := make([]interface{}, 1)
+	response := models.Response{
+		Code:    0,
+		Message: "",
+		Data:    nil,
+	}
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
+		response.Code = http.StatusInternalServerError
+		response.Message = "image获取失败！"
 	} else {
 		infoList = append(infoList, images)
-		c.JSON(http.StatusOK, infoList)
+		response.Code = http.StatusOK
+		response.Message = "image获取成功！"
+		response.Data = infoList
 	}
+	c.JSON(http.StatusOK, response)
 }
